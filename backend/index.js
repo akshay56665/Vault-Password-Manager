11bbc0a071d38f002510349f62f664d478303d2b
akshay56665 <156991241+akshay56665@users.noreply.github.com>
2024-08-27 18:40:18 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,7 @@ const cookieParser=require('cookie-parser')
 
 // mongoose.connect("mongodb://127.0.0.1:27017/vault")
 mongoose.connect(process.env.MONGO_URL)
-app.use(cors({
-    credentials:true,
-    origin:"https://vault-b929.onrender.com"})); 
+app.use(cors()); 
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
